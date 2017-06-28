42c9ae4deceffbdbe50d6bfcf30db7aa346bfe26
macarena baltra <macarena.bc2011@gmail.com>
2017-06-28 13:59:52 -0400

    Ejercicio Listo

--- js/main.js
+++ js/main.js
@@ -10,3 +10,15 @@ $(document).ready(function(){
     }
   });
 });
+
+
+/*
+Qué hace nuestra función?
+Una pequeña validación. Comenzamos diciendo que al hacer click en nuestro botón, se genere una función que:
+    - Tenga un preventDefault para que detenga la acción por defecto.
+    - Guardará los valores ingresados en las variables name y message.
+    - Y luego, hará una validación sencilla, para que nuestros campos no estén vacíos. Esto lo realizará una
+    sentencia if/else que:
+        - Si los campos nombre y mensaje están vacíos, debe generar una alerta para que el usuario los ingrese.
+        - Si no, mostrará en pantalla el nombre y mensaje ingresados por el usuario.
+*/
